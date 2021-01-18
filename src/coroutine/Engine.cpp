038d86d728031804c9cd0a76203555d193cc2bcc
Dmitry Lichmanov <dimlichmanov@gmail.com>
2021-01-18 15:17:54 +0300

    one more commit of 7 task

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -9,11 +9,13 @@ namespace Afina {
 namespace Coroutine {
 
 void Engine::Store(context &ctx) {
-    char Stack = 0;
-    if (&Stack > StackBottom) {
-        ctx.Hight = &Stack;
-    } else {
-        ctx.Low = &Stack;
+    char stack_end;
+    ctx.Hight = ctx.Low = StackBottom;
+    if (&stack_end > StackBottom) {
+        ctx.Hight = &stack_end;
+    }
+    else {
+        ctx.Low = &stack_end;
     }
     std::size_t stack_size = ctx.Hight - ctx.Low;
     if (stack_size > std::get<1>(ctx.Stack) || stack_size * 2 < std::get<1>(ctx.Stack)) {
@@ -22,34 +24,51 @@ void Engine::Store(context &ctx) {
         std::get<0>(ctx.Stack) = new char[stack_size];
     }
     std::memcpy(std::get<0>(ctx.Stack), ctx.Low, stack_size);
+
 }
 
 void Engine::Restore(context &ctx) {
+    char stack_end;
+    if ((&stack_end >= ctx.Low) && (&stack_end <= ctx.Hight)) {
+        Restore(ctx);
+    }
+
+    char* &buffer = std::get<0>(ctx.Stack);
     std::size_t stack_size = ctx.Hight - ctx.Low;
-    std::memcpy(ctx.Low, std::get<0>(ctx.Stack), stack_size);
+
+    memcpy(ctx.Low, buffer, stack_size);
     longjmp(ctx.Environment, 1);
 }
 
 void Engine::yield() {
-    auto it = alive;
-    if (it && it == cur_routine) {
-        it = it->next;
-    }
-    if (it) {
-        sched(it);
+    context * next_coro = alive;
+    if (next_coro != nullptr){
+        if (next_coro == cur_routine){
+            next_coro = next_coro ->next;
+        }
+        sched(next_coro);
     }
 }
 
 void Engine::sched(void *routine_) {
-    if (routine_ == nullptr or routine_ == cur_routine) {
-       yield();
+    if (cur_routine == routine_) {
+        return yield();
     }
-    if (setjmp(cur_routine -> Environment)){
-        return;
+
+    if (routine_) {
+        if (cur_routine && cur_routine != idle_ctx) {
+            if (setjmp(cur_routine->Environment) > 0) {
+                return;
+            }
+            Store(*cur_routine);
+        }
+
+        cur_routine = static_cast<context *>(routine_);
+        Restore(*cur_routine);
+    } else {
+        yield();
     }
-    Store(*cur_routine);
-    cur_routine = (context *)routine_;
-    Restore(*(context *)routine_);
+
 }
 
 } // namespace Coroutine
